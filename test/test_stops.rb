19b5a9fe00a67b75ac9fb0f810ca2a0e661ba681
bibenga <{ID}+{username}@users.noreply.github.com>
2025-11-03 12:06:31 +0000

    tests

---
+++ test/test_stops.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../lib/stop'
+
+class MaxIterationsTest < Minitest::Test
+  def test_done?
+    stop = ALNS::MaxIterations.new(10)
+    exp = 0
+    until stop.done?(nil, nil, nil)
+      #
+      exp += 1
+    end
+    assert_equal 10, exp
+  end
+end
+
+class MaxRuntimeTest < Minitest::Test
+  def test_done?
+    stop = ALNS::MaxRuntime.new(0.1)
+    started = Time.new
+    until stop.done?(nil, nil, nil) # rubocop:disable Style/WhileUntilModifier
+      sleep(0.001)
+    end
+    elapsed = ((Time.new - started) * 1000).to_i
+    assert_equal true, elapsed.between?(100, 105), "#{elapsed}"
+  end
+end
